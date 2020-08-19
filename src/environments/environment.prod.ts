706dfa5823b4d4b8d43fc8573cf8a4c93145dff1
Vasyl <kobetiakvasyl@gmail.com>
2020-08-19 21:51:53 +0300

    Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCU6EA2AAE5_Bxt6u9iYSsAwijPN8o1GmA',
+  fbDBUrl: 'https://angular-blog-a65b7.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyCU6EA2AAE5_Bxt6u9iYSsAwijPN8o1GmA',
+  fbDBUrl: 'https://angular-blog-a65b7.firebaseio.com'
 };
 
 /*
